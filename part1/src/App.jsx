b1aaa4cc57d96bbfff239e40b479b5793a25e22b
Nam Anh Dang <nd433@cornell.edu>
2024-05-27 07:01:40 -0400

    Finished 1b

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -9,46 +9,51 @@ const Header = ({ name }) => {
 };
 
 const Content = ({ list }) => {
-  console.log(list);
-  console.log(Object.keys(list));
   return (
     <>
-      {Object.keys(list).map((key) => (
+      {list.map(({ name, exercises }) => (
         <p>
-          {key.toString()} {list[key.toString()].toString()}
+          {name} {exercises}
         </p>
       ))}
     </>
   );
 };
 
-const Total = ({ exercises }) => {
+const Total = ({ list }) => {
   return (
     <>
       <p>
         Number of exercises{" "}
-        {exercises.reduce((acc, exercise) => acc + exercise, 0)}
+        {list.reduce((acc, { name, exercises }) => acc + exercises, 0)}
       </p>
     </>
   );
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header name={course}></Header>
-      <Content
-        list={{ [part1]: exercises1, [part2]: exercises2, [part3]: exercises3 }}
-      ></Content>
-      <Total exercises={[exercises1, exercises2, exercises3]}></Total>
+      <Header name={course.name}></Header>
+      <Content list={course.parts}></Content>
+      <Total list={course.parts}></Total>
     </div>
   );
 };
